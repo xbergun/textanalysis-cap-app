242fd856812b7f60aec1cb089f05f193f634ecbd
xbergun <berkay.ergun16@gmail.com>
2025-02-12 22:12:58 +0300

    Fix destination configuration and improve HuggingFace API integration in cat-service

--- srv/cat-service.ts
+++ srv/cat-service.ts
@@ -1,44 +1,28 @@
 import cds, { Request } from "@sap/cds";
+import { getDestination } from "@sap-cloud-sdk/connectivity";
 import axios from "axios";
 import { AnalyzeTextResponse, CustomError } from "./lib/types/global.types";
 
 export default cds.service.impl(async function () {
 
-    const destinationService2 = await cds.connect.to("HuggingFaceAPI");
-
-    console.log("DESTINATION SERVICE", destinationService2);
-
-
     this.on("analyzeText", async (req: Request) => {
-        //const destinationService = await cds.connect.to("destination");
-        const destinationService2 = await cds.connect.to("HuggingFaceAPI");
-
-
-        console.log("DESTINATION SERVICE", destinationService2);
-        const destination = await destinationService2.get("HuggingFaceAPI");
 
         const text: string = req.data.text;
 
+        const destinationService = await getDestination({
+            destinationName: "HuggingFaceAPI",
+        });
 
-        if (!destination || !destination.URL || !destination.HuggingFaceAPIKey) {
-            return req.error(500, "Failed to retrieve API credentials from destination");
-        }
-
-
-        const HF_API_KEY: string | undefined = process.env.HF_API_KEY;
-        const API_URL: string | undefined = process.env.API_URL;
-
-        if (!HF_API_KEY) {
-            return new CustomError("HF_API_KEY is not set", 400);
-        }
+        const { URL, HuggingFaceAPIKey } =
+            destinationService?.originalProperties || {};
 
         try {
             const response = await axios.post<AnalyzeTextResponse[]>(
-                destination.URL as string,
+                URL as string,
                 { inputs: text },
                 {
                     headers: {
-                        Authorization: destination.HuggingFaceAPIKey,
+                        Authorization: HuggingFaceAPIKey,
                         "Content-Type": "application/json",
                     },
                 }
